fb3ef17b81e7a3ec39067ce1a51b2ca88956947d
Ashley <adenisecort@gmail.com>
2023-08-16 20:58:47 -0500

    20.2 activity

---
+++ 19.1-activities/02_More_Variables/unsolved/js/index.js
@@ -0,0 +1,9 @@
+// Create the addNumbers function here.
+function addNumbers() {
+var numberTwo = 2;
+var numberThree = 3;
+var numberFive = numberTwo+numberThree;
+console.log (numberFive)
+}
+addNumbers();
+// Create the stringConcat function here.
